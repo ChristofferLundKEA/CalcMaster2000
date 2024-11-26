43ca5e837bcc281a8530211c334f65ef1bc173fb
ChristofferLundKEA <chlu0002@stud.kea.dk>
2024-11-26 14:16:39 +0100

    "In the beninging"

---
+++ src/main/java/com/alpha/solutions/calcmaster2000/CalcMaster2000Application.java
@@ -0,0 +1,13 @@
+package com.alpha.solutions.calcmaster2000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcMaster2000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalcMaster2000Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/alpha/solutions/calcmaster2000/CalcMaster2000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.solutions.calcmaster2000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcMaster2000ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
