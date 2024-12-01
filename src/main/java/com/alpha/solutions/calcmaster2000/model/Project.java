fb8ebcd5031baebc5fb4607e16899d620342d0b6
Celine Lund Månsson <166127204+Celinelundm@users.noreply.github.com>
2024-12-01 23:04:04 +0100

    Update Project.java
    Lægger alle attributter ind og tilhørende gettere og settere

--- src/main/java/com/alpha/solutions/calcmaster2000/model/Project.java
+++ src/main/java/com/alpha/solutions/calcmaster2000/model/Project.java
@@ -1,4 +1,52 @@
 package com.alpha.solutions.calcmaster2000.model;
 
+import java.util.Date;
+
 public class Project {
+
+    private int projectID;
+    private String name;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+
+    public int getProjectID() {
+        return projectID;
+    }
+
+    public void setProjectID(int projectID) {
+        this.projectID = projectID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 }
