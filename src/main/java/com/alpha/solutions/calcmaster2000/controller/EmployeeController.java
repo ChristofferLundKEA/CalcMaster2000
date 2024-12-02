b0b36b2387df63c95d382d9f05effd5eac97f164
ChristofferLundKEA <chlu0002@stud.kea.dk>
2024-12-02 13:48:03 +0100

    Added full employee list

--- src/main/java/com/alpha/solutions/calcmaster2000/controller/EmployeeController.java
+++ src/main/java/com/alpha/solutions/calcmaster2000/controller/EmployeeController.java
@@ -1,7 +1,26 @@
 package com.alpha.solutions.calcmaster2000.controller;
 
+import com.alpha.solutions.calcmaster2000.model.Employee;
+import com.alpha.solutions.calcmaster2000.service.EmployeeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public String listAllEmployees(Model model){
+        List<Employee> employees = employeeService.getAllEmployees();
+        model.addAttribute("employees", employees);
+        return "employees";
+    }
 }
--- src/main/java/com/alpha/solutions/calcmaster2000/service/EmployeeService.java
+++ src/main/java/com/alpha/solutions/calcmaster2000/service/EmployeeService.java
@@ -1,7 +1,21 @@
 package com.alpha.solutions.calcmaster2000.service;
 
+import com.alpha.solutions.calcmaster2000.model.Employee;
+import com.alpha.solutions.calcmaster2000.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.getAllEmployees();
+    }
 }
