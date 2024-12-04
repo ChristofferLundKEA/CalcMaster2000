eea805c1b22ee8686516c69bd8634c846efb4553
ChristofferLundKEA <chlu0002@stud.kea.dk>
2024-12-04 17:36:23 +0100

    CRUD for employees employee profile

--- src/main/java/com/alpha/solutions/calcmaster2000/controller/EmployeeController.java
+++ src/main/java/com/alpha/solutions/calcmaster2000/controller/EmployeeController.java
@@ -5,6 +5,9 @@ import com.alpha.solutions.calcmaster2000.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -22,5 +25,43 @@ public class EmployeeController {
         List<Employee> employees = employeeService.getAllEmployees();
         model.addAttribute("employees", employees);
         return "employees";
+    }
+
+    @GetMapping("/profile/{employeeID}")
+    public String employeeProfile(@PathVariable int employeeID, Model model){
+        Employee emp = employeeService.getEmployeeByID(employeeID);
+        model.addAttribute("employee", emp);
+        return "profile";
+    }
+
+    @GetMapping("/createEmployee")
+    public String createEmployeeForm(Model model){
+        model.addAttribute("employee", new Employee());
+        return "createEmployee";
+    }
+
+    @PostMapping("/createEmployee")
+    public String handleCreateEmployeeForm(@ModelAttribute Employee employee){
+        employeeService.addNewEmployee(employee);
+        return "redirect:/employees";
+    }
+
+    @PostMapping("/deleteEmployee/{id}")
+    public String deleteEmployeeById(@PathVariable int id){
+        employeeService.deleteEmployeeById(id);
+        return "redirect:/employees";
+    }
+
+    @GetMapping("/updateEmployee/{id}")
+    public String editEmployee(@PathVariable int id, Model model){
+        Employee employee = employeeService.getEmployeeByID(id);
+        model.addAttribute("employee", employee);
+        return "editEmployee";
+    }
+
+    @PostMapping("/updateEmployee")
+    public String updateEmployee(@ModelAttribute Employee employee){
+        employeeService.updateEmployee(employee);
+        return "redirect:/employees";
     }
 }
